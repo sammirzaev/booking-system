20f4c51aacf856746345f1296fb7f001037b4c44
Sukhrab T <sukhrab@mail.ru>
2019-10-15 13:36:40 +0500

    front location child item remove

--- resources/views/layouts/reservation/location-child.blade.php
+++ resources/views/layouts/reservation/location-child.blade.php
@@ -3,7 +3,7 @@
     @if($locations->where('parent_id', $item->id)->isNotEmpty())
         @include('layouts.reservation.location-child', [
            'items' => $locations->where('parent_id', $item->id)->sortBy('sort'),
-           'parent' => $item->parent_id ? $parent . $item->language->title . ' > ' : $item->language->title . ' > '
+           'parent' => $item->parent_id ? $parent . ' ' : ' '
        ])
     @endif
 @endforeach
