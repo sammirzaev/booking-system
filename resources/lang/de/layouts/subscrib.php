fb6043f1238777ca75d919486104860fab377586
Sukhrab T <sukhrab@mail.ru>
2019-10-16 21:00:14 +0500

    trans fix

---
+++ resources/lang/de/layouts/subscrib.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Subscribe Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'title' => 'Beste Neigkeiten hier, bitte melden Sie sich an',
+    'text' => 'Um die letzten Neuigkeiten zu bekommen, fuehren Sie bitte hier die Registration ein!',
+    'placeholder' => 'E-mail-addresse bitte eingeben',
+    'button' => 'Bitte sich anmelden',
+
+];
